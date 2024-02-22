d019175fe36579759ad8d1b844fa254401412f97
Fari98 <71496424+Fari98@users.noreply.github.com>
2024-02-22 12:11:29 +0000

    useful resource

---
+++ resources/tree/population.py
@@ -0,0 +1,20 @@
+from tree.utils.utils import flatten
+
+class Population():
+
+
+    def __init__(self, pop):
+
+
+        self.pop = pop
+        self.size = len(pop)
+
+        self.nodes_count = sum([len(list(flatten(ind.repr_))) for ind in pop])
+
+    def evaluate(self, solver, std_params, test = False):
+
+
+        self.fit = [individual.evaluate_tree(solver, std_params, test)
+                    for individual in self.pop]
+
+        self.nodes = sum([individual.nodes for individual in self.pop])
