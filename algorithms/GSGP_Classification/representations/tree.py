2b4bb906b44c3f53953fc6427abd1f2551278c8a
DiogoRasteiro <34631781+DiogoRasteiro@users.noreply.github.com>
2024-05-18 19:07:32 +0100

    Tree class skeleton

---
+++ algorithms/GSGP_Classification/representations/tree.py
@@ -0,0 +1,18 @@
+
+
+
+class Tree:
+    LABELS = None
+    PERCENTILES = None
+
+    def __init__(self, structure, train_semantics, test_semantics, reconstruct):
+        if structure is not None and reconstruct:
+            self.structure = structure  # either repr_ from gp(tuple) or list of pointers
+
+        self.train_semantics = train_semantics
+        self.test_semantics = test_semantics
+
+        self.fitness = None
+        self.test_fitness = None
+
+        # TODO: Depth calculation in utils and call it here
