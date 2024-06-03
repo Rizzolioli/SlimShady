f6d1bdf216d29f65d6022fad2afbb5f855bb3ddb
gpietrop <gloria.pietropolli@gmail.com>
2024-06-03 12:31:11 +0200

    fix test according to the new class implementation.

--- test/test_main_function.py
+++ test/test_main_function.py
@@ -1,9 +1,18 @@
-from slim.main_gp import gp
+from slim.main_gp import gp  # import the slim library
+from datasets.data_loader import load_ppb  # import the loader for the dataset PPB
+from slim.evaluators.fitness_functions import rmse  # import the rmse fitness metric
+from slim.utils.utils import train_test_split  # import the train-test split function
 
-datasets = ["toxicity"]
-n_runs = 1
-pop_size = 100
-n_iter = 100
-p_xo = 0.8
+X, y = load_ppb(X_y=True)  # load the PPB dataset
 
-gp(datasets=datasets, n_runs=n_runs, pop_size=pop_size, n_iter=n_iter, p_xo=p_xo)
+X_train, X_test, y_train, y_test = train_test_split(X, y, p_test=0.4)  # split into train and test
+X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, p_test=0.5)  # split into the test and validation
+
+# apply the gp algorithm
+final_tree = gp(X_train=X_train, y_train=y_train,
+                X_test=X_val, y_test=y_val,
+                dataset_name='ppb', pop_size=100, n_iter=100)
+
+final_tree.print_tree_representation()  # show the best individual structure at last generation
+predictions = final_tree.predict(X_test)  # get the prediction of the best individual in the test set
+print(float(rmse(y_true=y_test, y_pred=predictions)))  # compute metrics on the test set
