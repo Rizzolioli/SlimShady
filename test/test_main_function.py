88bb668c99b362b96b35759c11cca09e1473e62f
gpietrop <gloria.pietropolli@gmail.com>
2024-06-04 10:07:45 +0200

    update test example.

--- test/test_main_function.py
+++ test/test_main_function.py
@@ -1,4 +1,4 @@
-from slim.main_gp import gp  # import the slim library
+from slim.main_gsgp import gsgp  # import the slim library
 from datasets.data_loader import load_ppb  # import the loader for the dataset PPB
 from slim.evaluators.fitness_functions import rmse  # import the rmse fitness metric
 from slim.utils.utils import train_test_split  # import the train-test split function
@@ -9,9 +9,9 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, p_test=0.4)  # split i
 X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, p_test=0.5)  # split into the test and validation
 
 # apply the gp algorithm
-final_tree = gp(X_train=X_train, y_train=y_train,
-                X_test=X_val, y_test=y_val,
-                dataset_name='ppb', pop_size=100, n_iter=100)
+final_tree = gsgp(X_train=X_train, y_train=y_train,
+                  X_test=X_val, y_test=y_val,
+                  dataset_name='ppb', pop_size=100, n_iter=100)
 
 final_tree.print_tree_representation()  # show the best individual structure at last generation
 predictions = final_tree.predict(X_test)  # get the prediction of the best individual in the test set
