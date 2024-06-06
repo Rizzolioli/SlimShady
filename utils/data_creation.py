7b5202c7ca02aa21dde6c3d6784cd2046d5cf6a7
Fari98 <71496424+Fari98@users.noreply.github.com>
2024-06-06 09:09:23 +0100

    mut step

---
+++ utils/data_creation.py
@@ -0,0 +1,72 @@
+import scipy.optimize
+from scipy.optimize import rosen
+import numpy as np
+import math
+from sklearn.preprocessing import MinMaxScaler
+
+def rastrigin(matrix):
+    """
+    Calculate the Rastrigin function value for each row in the matrix.
+    """
+    n_rows, n_cols = matrix.shape
+    fitness_values = np.zeros(n_rows)
+    for i in range(n_rows):
+        position = matrix[i]
+        fitness_val = 0.0
+        for xi in position:
+            fitness_val += (xi * xi) - (10 * math.cos(2 * math.pi * xi)) + 10
+        fitness_values[i] = fitness_val
+    return fitness_values
+
+def sphere(matrix):
+    n_rows, n_cols = matrix.shape
+    fitness_values = np.zeros(n_rows)
+    for i in range(n_rows):
+        position = matrix[i]
+        fitness_val = 0.0
+        for xi in position:
+            fitness_val += (xi * xi)
+        fitness_values[i] = fitness_val
+    return fitness_values
+
+def create_random_matrix(n, m, ranges):
+    if len(ranges) != m:
+        raise ValueError("Length of ranges list must be equal to the number of columns (m).")
+    matrix = np.zeros((n, m))
+    for i in range(m):
+        min_val, max_val = ranges[i]
+        matrix[:, i] = np.random.uniform(min_val, max_val, n)
+    return matrix
+
+
+def create_dataset(rows, columns, scale_inputs, scale_output, function):
+
+    #TODO fix seed
+
+    functions = {'rastrigin' : rastrigin,
+                 'sphere' : sphere,
+                 'rosenbrock' : scipy.optimize.rosen}
+
+    if function not in functions.keys():
+        raise ValueError('Invalid function')
+
+    X = create_random_matrix(rows, columns, [scale_inputs for _ in range(10)])
+    if function == 'rosenbrock':
+        X = X.T
+
+    scaler = MinMaxScaler(scale_output)
+    y = scaler.fit_transform(functions[function](X).reshape(-1, 1))
+
+    return X, y.flatten()
+
+
+
+
+
+
+
+
+data = create_random_matrix(100, 10, [[0,1] for _ in range(10)])
+np.concatenate((data, rastrigin(data).reshape(-1,1)), axis = 1).shape
+
+
