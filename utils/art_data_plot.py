d801d3eae89bc343613df4f699be26f57bcfe944
Fari98 <71496424+Fari98@users.noreply.github.com>
2024-06-26 11:44:38 +0100

    Update art_data_plot.py

--- utils/art_data_plot.py
+++ utils/art_data_plot.py
@@ -1,27 +1,52 @@
 from data_creation import create_dataset
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+import time
 
 
 
-for data_function in ['rastrigin', 'sphere', 'rosenbrock' ]:
+for data_function in ['rastrigin', 'sphere', 'rosenbrock']:
 
     for (input_scale, output_scale) in [((0,1), (0,1)),
-                                        # ((0,1), (0,10)), ((0,1), (0,100)), ((0,1), (0,1000)),
-                                        # ((0, 10), (0, 1)), ((0, 100), (0, 1)),((0, 1000), (0, 1)),
-                                        # ((0,10), (0,10)), ((0,100), (0,100)), ((0,1000), (0,1000))
+                                        ((0,1), (0,10)), ((0,1), (0,100)), ((0,1), (0,1000)),
+                                        ((0, 10), (0, 1)), ((0, 100), (0, 1)),((0, 1000), (0, 1)),
+                                        ((0,10), (0,10)), ((0,100), (0,100)), ((0,1000), (0,1000))
                                         ]:
 
 
-        X, y = create_dataset(10000, 2, scale_inputs = input_scale, scale_output = output_scale,
+        X, z = create_dataset(10000, 2, scale_inputs = input_scale, scale_output = output_scale,
                               function = data_function, seed = 1)
+
+        x = X[:,0]
+        y = X[:, 1]
 
         # plt.close()
 
-        fig = plt.figure(figsize=(12, 12))
-        ax = fig.add_subplot(projection='3d')
+        # Create 2D grid coordinates for surface plot
+        xi = np.linspace(x.min(), x.max(), 100)
+        yi = np.linspace(y.min(), y.max(), 100)
+        xi, yi = np.meshgrid(xi, yi)
 
+        # Interpolate to get z values on grid
+        from scipy.interpolate import griddata
 
+        zi = griddata((x, y), z, (xi, yi), method='cubic')
 
-        ax.scatter(X[:, 0], X[:, 1], y)
-        # plt.savefig()
-        plt.show()
+        # Plot
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot_surface(xi, yi, zi, cmap=cm.viridis_r)
+        ax.view_init(elev=45, azim=30)
+
+        ax.set_xlabel('X1')
+        ax.set_ylabel('X2')
+        ax.set_zlabel('Y')
+
+        plt.title(f'{data_function}  INP:{input_scale}  OUT:{output_scale}')
+
+        plt.show(block = False)
+        time.sleep(3)
+        plt.close()
