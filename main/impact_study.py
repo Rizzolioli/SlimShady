159d926681d681d26a376a6fe801d0146905391a
Davide Farinati <71496424+Fari98@users.noreply.github.com>
2024-07-16 13:44:10 +0100

    Update impact_study.py

--- main/impact_study.py
+++ main/impact_study.py
@@ -21,31 +21,33 @@ slim_dataset_params = {"toxicity": {"p_inflate": 0.1, "ms": generate_random_unif
                        "concrete_strength": {"p_inflate": 0.5, "ms": generate_random_uniform(0, 0.3)},
                        "other": {"p_inflate": 0.3, "ms": generate_random_uniform(0, 1)}}
 
-for seed in range(10):
-    for loader in data_loaders:
+for seed in range(100):
+    for algo in [ (True, 'sum', False), (False,  "mul", True), (False,  "mul", False)]: #
+        for loader in data_loaders:
 
-        X, y = loader(X_y=True)
+            X, y = loader(X_y=True)
 
-        # getting the name of the dataset
-        dataset = loader.__name__.split("load_")[-1]
+            # getting the name of the dataset
+            dataset = loader.__name__.split("load_")[-1]
 
-        # getting the terminals and defining the terminal-dependant parameters
-        TERMINALS = get_terminals(loader)
-        # Performs train/test split
-        X_train, X_test, y_train, y_test = train_test_split(X=X, y=y,
-                                                            p_test=0.2,
-                                                            seed=seed)
+            # getting the terminals and defining the terminal-dependant parameters
+            TERMINALS = get_terminals(loader)
+            # Performs train/test split
+            X_train, X_test, y_train, y_test = train_test_split(X=X, y=y,
+                                                                p_test=0.2,
+                                                                seed=seed)
 
-        create_random_slim_ind( blocks = 1000,
-                                   X_train = X_train,
-                                   X_test = X_test,
-                                   FUNCTIONS = FUNCTIONS,
-                                   TERMINALS = TERMINALS,
-                                   CONSTANTS = CONSTANTS,
-                                   dataset_name = dataset,
-                                   y_train = None,
-                                   y_test = None,
-                                   algorithm = (True, 'sum', False),
-                                   mutation_step = slim_dataset_params[dataset]["ms"] if dataset in slim_dataset_params.keys() else  slim_dataset_params["other"]["ms"],
-                                   initial_depth = 6, seed = seed,
-                                   log = 1, log_path = 'log/deep_mutation_impact.csv', verbose = 1)
+            create_random_slim_ind( blocks = 1000,
+                                       X_train = X_train,
+                                       X_test = X_test,
+                                       FUNCTIONS = FUNCTIONS,
+                                       TERMINALS = TERMINALS,
+                                       CONSTANTS = CONSTANTS,
+                                       deflate_on = 1000,
+                                       dataset_name = dataset,
+                                       y_train = None,
+                                       y_test = None,
+                                       algorithm = algo,
+                                       mutation_step = slim_dataset_params[dataset]["ms"] if dataset in slim_dataset_params.keys() else  slim_dataset_params["other"]["ms"],
+                                       initial_depth = 6, seed = seed,
+                                       log = 1, log_path = 'log/deep_mutation_impact_n.csv', verbose = 1)
