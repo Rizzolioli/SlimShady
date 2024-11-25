fixed sign ffunction

algorithms/SLIM_GSGP/operators/mutators.py
@@ -234,7 +234,7 @@ def weighted_deflate_mutation(metric, selection = np.random.choice):
         limit = 1 if allow_bt else 2
 
         probabilities = [metric(individual.head_signed_error, block).item() for block in individual.train_semantics[1:individual.size - limit + 1 ]]
-        probabilities = [p/sum(probabilities) for p in probabilities]
+        probabilities = [p/sum(probabilities) for p in probabilities] if sum(probabilities) > 0 else None
         idxs = [i for i in range(1,  individual.size - limit + 1 )]
 
         if individual.size > limit:

evaluators/fitness_functions.py
@@ -20,7 +20,7 @@ def signed_errors(y_true, y_pred):
 def sign(y_true, y_pred):
     sign_y_true = torch.greater_equal(y_true, 0)
     sign_y_pred = torch.greater_equal(y_pred, 0)
-    return torch.sum(torch.eq(sign_y_true, sign_y_pred))
+    return torch.sum(torch.ne(sign_y_true, sign_y_pred))
 
 def sign_rmse(y_true, y_pred):
     return torch.add(rmse(y_true, y_pred), torch.mul(torch.div(rmse(y_true, y_pred), y_true.size()[0]), sign(y_true,y_pred)))