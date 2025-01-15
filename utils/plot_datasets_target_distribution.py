f08a412d897081abc3333ed7c8cfd1e5f12b60e1
Davide Farinati <71496424+Fari98@users.noreply.github.com>
2025-01-15 08:45:09 +0000

    updates

--- utils/plot_datasets_target_distribution.py
+++ utils/plot_datasets_target_distribution.py
@@ -1,5 +1,6 @@
 from datasets.data_loader import *
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 import numpy as npù
 import torch
 
@@ -13,7 +14,14 @@ for loader in data_loaders:
     # print(torch.median(y).item())
     # print(torch.std(y).item())
     plt.title(loader.__name__.split("load_")[-1])
-    plt.savefig(f'input_distr/{loader.__name__.split("load_")[-1]}.png')
+    # plt.savefig(f'input_distr/{loader.__name__.split("load_")[-1]}.png')
     plt.show()
     plt.close()
-    #
+
+    y = StandardScaler().fit_transform(y.reshape(-1, 1))
+    plt.hist(y)
+    plt.title(loader.__name__.split("load_")[-1] + '_scaled')
+    # plt.savefig(f'input_distr/{loader.__name__.split("load_")[-1]}.png')
+    plt.show()
+    plt.close()
+
