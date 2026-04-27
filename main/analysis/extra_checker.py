3811a83b681df0160e6c316c4ebb55b942bc44d2
Davide Farinati <71496424+Fari98@users.noreply.github.com>
2026-04-27 15:48:53 +0200

    functions

---
+++ main/analysis/extra_checker.py
@@ -0,0 +1,24 @@
+import random
+import time
+import uuid
+import sys, os; sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+
+from parametrization import *
+from algorithms.SLIM_GSGP.slim_gsgp import SLIM_GSGP
+import datasets.data_loader as ds
+from utils.utils import get_terminals, train_test_split
+from algorithms.SLIM_GSGP.operators.mutators import *
+from utils.logger import log_settings
+from utils.utils import show_individual
+
+dataset = "energy"
+
+curr_dataset = f"load_{dataset}"
+
+TERMINALS = get_terminals(dataset, 0 + 1)
+
+X_train, y_train = load_preloaded(dataset, seed=0 + 1, training=True, X_y=True)
+
+X_test, y_test = load_preloaded(dataset, seed=0 + 1, training=False, X_y=True)
+
+print(X_test)
